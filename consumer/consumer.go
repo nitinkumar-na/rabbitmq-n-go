347a68ff60dcdb062c710835d9b26824577f5754
nitinkumar-na <nitin.kumar@netapp.com>
2024-12-30 11:57:47 +0530

    feat: producer consumer scaffolding

---
+++ consumer/consumer.go
@@ -0,0 +1,7 @@
+package consumer
+
+import "fmt"
+
+func TestConsumer() {
+	fmt.Println("Hello From Consumer")
+}
--- main.go
+++ main.go
@@ -1,5 +1,11 @@
 package main
 
-func main() {
+import (
+	"github.com/nitinkumar-na/rabbitmq-with-go/consumer"
+	"github.com/nitinkumar-na/rabbitmq-with-go/producer"
+)
 
+func main() {
+	consumer.TestConsumer()
+	producer.TestProducer()
 }
