5efbf6a5176a4ecc975a31acac6891fb7fb9262f
nitinkumar-na <nitin.kumar@netapp.com>
2025-01-02 10:22:06 +0530

    feat: user input

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"github.com/nitinkumar-na/rabbitmq-n-go/consumer"
+	"github.com/nitinkumar-na/rabbitmq-n-go/helper"
 	"github.com/nitinkumar-na/rabbitmq-n-go/producer"
 )
 
@@ -13,6 +15,28 @@ import (
 
 // 2. Take user input, publish and consumer prints
 func main() {
-	producer.PublishMessage("Hello World")
-	consumer.ListenToMessages()
+
+	for {
+		input := getUserInput()
+
+		if input == "exit" {
+			break
+		}
+
+		producer.PublishMessage(input)
+		consumer.ListenToMessages()
+	}
+}
+
+func getUserInput() string {
+	fmt.Println("Enter your message, or type 'exit' to quit")
+	var input string
+	fmt.Scan(&input)
+
+	if helper.IsBlank(input) {
+		fmt.Println("Message can not be empty")
+		getUserInput()
+	}
+
+	return input
 }
