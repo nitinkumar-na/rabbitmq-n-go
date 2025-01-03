c8aff3bd3830fe5abf463d191bd1bdd4fc439956
nitinkumar-na <nitin.kumar@netapp.com>
2025-01-03 22:34:14 +0530

    refactor: code cleanup

--- helper/helper.go
+++ helper/helper.go
@@ -1,9 +1,17 @@
 package helper
 
 import (
+	"fmt"
+	amqp "github.com/rabbitmq/amqp091-go"
 	"log"
 	"strings"
 )
+
+type RabbitMqConn struct {
+	Conn    *amqp.Connection
+	Channel *amqp.Channel
+	Queue   amqp.Queue
+}
 
 func HandleError(err error, msg string) {
 	if err != nil {
@@ -18,4 +26,47 @@ func IsBlank(str string) bool {
 
 func IsNotBlank(str string) bool {
 	return !IsBlank(str)
+}
+
+func StartRabbitMqClient(client string) RabbitMqConn {
+	fmt.Println(client, "Starting...")
+
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	HandleError(err, client+" Failed to connect to RabbitMQ")
+	//defer conn.Close()
+
+	ch, err := conn.Channel()
+	HandleError(err, client+" Failed to open a channel")
+	//defer ch.Close()
+
+	queue, err := ch.QueueDeclare(
+		"my-queue",
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+	HandleError(err, client+" Failed to create q queue")
+
+	fmt.Println(client, " Started Successfully")
+
+	return RabbitMqConn{conn, ch, queue}
+}
+
+// ConsumerMessages returns a read only channel
+func ConsumerMessages(client *RabbitMqConn) <-chan amqp.Delivery {
+	msgs, err := client.Channel.Consume(
+		client.Queue.Name,
+		"",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+
+	HandleError(err, "Failed to register a consumer")
+
+	return msgs
 }
